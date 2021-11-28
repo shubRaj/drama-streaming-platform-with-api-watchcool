e9df37bd512e5073628a70ed1cc363866756076a
shubraj <shuvraj1234@gmail.com>
2021-11-28 23:35:49 +0545

    notification fixed

--- startServer.py
+++ startServer.py
@@ -1,2 +1,2 @@
 import os
-os.system(f"gunicorn watchcool.wsgi -b=127.0.0.1:8005 --timeout={60*5} --workers=5 --threads=2 --daemon")
+os.system(f"gunicorn watchcool.wsgi -b=127.0.0.1:8005 --timeout={60*5} --workers=5 --daemon")
