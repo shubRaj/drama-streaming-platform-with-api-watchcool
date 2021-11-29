257c52bc9412b5ec0119f6887b874d8550b17a7c
shubraj <shuvraj1234@gmail.com>
2021-11-29 22:42:29 +0545

    stream api changed

--- appapi/urls.py
+++ appapi/urls.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import cache_page
 app_name = "app_appapi"
 urlpatterns = [
     path("settings/",cache_page(settings.CACHE_TIME)(views.ConfigAPIView.as_view()),name="settings"),
-    path("search/<str:query>/",views.SearchAPIView.as_view(),name="search"),
+    path("search/<str:query>/",cache_page(settings.CACHE_TIME)(views.SearchAPIView.as_view()),name="search"),
     path("series/show/<int:pk>/",views.SerieAPIView.as_view(),name="serie"),
     path("media/detail/<int:pk>/",views.MovieAPIView.as_view(),name="movie"),
     path("series/relateds/<int:pk>/",cache_page(settings.CACHE_TIME)(views.RelatedSerieAPIView.as_view()),name="related_series"),
