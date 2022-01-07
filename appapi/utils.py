d708b96cb2669472b1fd0c0377329d066c63ee7a
shubraj <shuvraj1234@gmail.com>
2022-01-07 23:19:08 +0545

    external episode

--- appapi/utils.py
+++ appapi/utils.py
@@ -154,7 +154,7 @@ def singleEpisode(episode:dict,backdrop_path="http://image.tmdb.org/t/p/w500/Non
             watchepisode['status'] = 1
             watchepisode["created_at"] = watchepisode.pop("added_on")
             watchepisode["updated_at"] = watchepisode["created_at"]
-            if (watchepisode["server"] == "XStreamCDN") or (watchepisode["server"] == "StreamX"):
+            if (watchepisode["server"] == "XStreamCDN"):
                 downloads = copy.copy(watchepisode)
                 downloads.pop("hls")
                 downloads.pop("embed")
