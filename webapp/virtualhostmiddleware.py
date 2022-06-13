08e774891e843927bba1faad4a53dd50240c3429
Shub Raj Lama <shuvraj1234@gmail.com>
2022-06-13 16:42:06 +0545

    vh added

--- webapp/virtualhostmiddleware.py
+++ webapp/virtualhostmiddleware.py
@@ -1,13 +1,17 @@
-
+virtual_hosts = {
+    "www.watchcool.in":"watchcool.webapp_urls",
+    "watchcool.in":"watchcool.webapp_urls",
+    "coolapi.watchcool.in":"watchcool.api_urls",
+    "myapi.watchcool.in":"watchcool.api_urls",
+    "api.watchcool.in":"watchcool.api_urls",
+    "localhost:8000":"watchcool.webapp_urls"
+}
 class VirtualHostMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         host = request.get_host()
-        if  "localhost" in  host or host in "www.watchcool.in":
-            request.urlconf = "watchcool.webapp_urls"
-        else:
-            request.urlconf = "watchcool.api_urls"
+        request.urlconf = virtual_hosts.get(host,"watchcool.webapp_urls")
         response = self.get_response(request)
         return response
